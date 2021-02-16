792fd73812acec90ad8925af2408c32394a9acc4
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-16 09:27:04 -0600

    commit tree a6261ec8660e1b3fccb4dc3a80e9eea5373d3824

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,39 @@
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all
-	end
+  def index
+    @posts = Post.all
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def show
+    find_post
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
-	end
+  def create
+    @post = Post.new(post_params(:title, :description))
+    @post.save
+    redirect_to post_path(@post)
+  end
 
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
+  def update
+    find_post
+    @post.update(post_params(:title))
+    redirect_to post_path(@post)
+  end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
 end
